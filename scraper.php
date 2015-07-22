0f7b3614d602bf79a57a55b109c4dc3f6b7287b7
Dave Shumway <davidshumway@gmail.com>
2015-07-21 21:40:14 -0400

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -18,7 +18,7 @@ require 'scraperwiki.php';
 //var_dump(scraperwiki);
 //var_dump(scraperwiki::);
 //var_dump(scraperwiki::sqliteexecute);
-scraperwiki::save_sqlite(array("reqname", "title"), array(array('Hi ', "Hi "),array('there','is')), 'tasks');
+echo scraperwiki::save_sqlite(array("reqname", "title"), array(array('Hi ', "Hi "),array('there','is')), 'tasks');
 //scraperwiki::save_sqlite(["reqname__tasktitle"], data={"reqname__tasktitle"=>'yy', "reqname"=>"Hi there"});
 //scraperwiki::save_sqlite(["reqname__tasktitle"], data={"reqname__tasktitle"=>'zz', "reqname"=>"Oh"});
 
